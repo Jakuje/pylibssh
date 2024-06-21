24a31cb389cfb509eeb8366633aa4d5d5c065657
Jakub Jelen <jjelen@redhat.com>
2024-06-21 11:58:26 +0200

    test for setting log level
    Signed-off-by: Jakub Jelen <jjelen@redhat.com>

--- tests/unit/session_test.py
+++ tests/unit/session_test.py
@@ -2,10 +2,16 @@
 
 """Tests suite for session."""
 
+import logging
+
 import pytest
 
 from pylibsshext.errors import LibsshSessionException
+from pylibsshext.logging import ANSIBLE_PYLIBSSH_TRACE
 from pylibsshext.session import Session
+
+
+LOCALHOST = '127.0.0.1'
 
 
 def test_make_session():
@@ -18,4 +24,49 @@ def test_session_connection_refused(free_port_num):
     error_msg = '^ssh connect failed: Connection refused$'
     ssh_session = Session()
     with pytest.raises(LibsshSessionException, match=error_msg):
-        ssh_session.connect(host='127.0.0.1', port=free_port_num)
+        ssh_session.connect(host=LOCALHOST, port=free_port_num)
+
+
+def test_session_log_level_debug(caplog, free_port_num):
+    """Test setting the log level to DEBUG should reveal copyright information."""
+    ssh_session = Session()
+    ssh_session.set_log_level(logging.DEBUG)
+
+    # the connection will fail but first log lands before that
+    with pytest.raises(LibsshSessionException):
+        ssh_session.connect(host=LOCALHOST, port=free_port_num)
+
+    found_copyright = False
+    for record in caplog.records:
+        # This log message is shown at different log levels in different libssh versions
+        if record.levelname in {'DEBUG', 'INFO'} and 'and libssh contributors.' in record.msg:
+            found_copyright = True
+    assert found_copyright
+
+
+def test_session_log_level_no_log(caplog, free_port_num):
+    """Test setting the log level to NONE should be quiet."""
+    ssh_session = Session()
+    ssh_session.set_log_level(logging.NOTSET)
+
+    # the connection will fail but first log lands before that
+    with pytest.raises(LibsshSessionException):
+        ssh_session.connect(host=LOCALHOST, port=free_port_num)
+
+    assert not caplog.records
+
+
+def test_session_log_level_trace(caplog, free_port_num):
+    """Test setting the log level to TRACE should provide even more logs."""
+    ssh_session = Session()
+    ssh_session.set_log_level(ANSIBLE_PYLIBSSH_TRACE)
+
+    # the connection will fail but first log lands before that
+    with pytest.raises(LibsshSessionException):
+        ssh_session.connect(host=LOCALHOST, port=free_port_num)
+
+    found_trace = False
+    for record in caplog.records:
+        if record.levelname == 'TRACE' and 'ssh_socket_pollcallback: Poll callback on socket' in record.msg:
+            found_trace = True
+    assert found_trace
