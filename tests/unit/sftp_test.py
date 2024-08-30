4516fab71b7ecdb3ae1fc3ef9b4bef133c7385b7
Jakub Jelen <jjelen@redhat.com>
2024-08-30 11:20:00 +0200

    tests: Reproducer for #341
    Signed-off-by: Jakub Jelen <jjelen@redhat.com>

--- tests/unit/sftp_test.py
+++ tests/unit/sftp_test.py
@@ -2,6 +2,8 @@
 
 """Tests suite for sftp."""
 
+import random
+import string
 import uuid
 
 import pytest
@@ -63,3 +65,29 @@ def test_get(dst_path, src_path, sftp_session, transmit_payload):
     """Check that SFTP file download works."""
     sftp_session.get(str(src_path), str(dst_path))
     assert dst_path.read_bytes() == transmit_payload
+
+
+@pytest.fixture
+def large_payload():
+    """Generate a large 1025 byte (1024 + 1B) test payload."""
+    payload_len = 1024 + 1
+    random_bytes = [ord(random.choice(string.printable)) for _ in range(payload_len)]
+    return bytes(random_bytes)
+
+
+@pytest.fixture
+def src_path_large(tmp_path, large_payload):
+    """Return a remote path to a 1025 byte-sized file.
+
+    The pylibssh chunk size is 1024 so the test needs a file that would
+    execute at least two loops.
+    """
+    path = tmp_path / 'large.txt'
+    path.write_bytes(large_payload)
+    return path
+
+
+def test_get_large(dst_path, src_path_large, sftp_session, large_payload):
+    """Check that SFTP can download large file."""
+    sftp_session.get(str(src_path_large), str(dst_path))
+    assert dst_path.read_bytes() == large_payload
