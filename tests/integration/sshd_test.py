a93c5d141ba554fc73a4c0b4e2f76a9decb0ca1f
Sviatoslav Sydorenko <webknjaz@redhat.com>
2025-10-18 18:37:18 +0200

    🧪 Address ARG001 unused-function-argument

--- tests/integration/sshd_test.py
+++ tests/integration/sshd_test.py
@@ -1,9 +1,13 @@
 """Sanity tests for sshd-related helpers."""
 
+import pytest
+
+
 MAX_PORT_NUMBER = 65535
 
 
-def test_sshd_addr_fixture_port(sshd_addr, ssh_client_session):
+@pytest.mark.usefixtures('ssh_client_session')
+def test_sshd_addr_fixture_port(sshd_addr):
     """Smoke-test sshd_addr fixture.
 
     # noqa: DAR101
