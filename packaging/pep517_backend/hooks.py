15485a1a1421d9e8b35c0877337654e6a7fe42e4
Sviatoslav Sydorenko <webknjaz@redhat.com>
2025-10-18 17:56:16 +0200

    🧪 Suppress F401 @ PEP 517 backend entry point

--- packaging/pep517_backend/hooks.py
+++ packaging/pep517_backend/hooks.py
@@ -8,10 +8,10 @@ from setuptools.build_meta import *  # noqa: E501, F403, WPS347
 
 # Re-exporting PEP 517 hooks
 from ._backend import (  # type: ignore[assignment]
-    build_sdist,
-    build_wheel,
-    get_requires_for_build_wheel,
-    prepare_metadata_for_build_wheel,
+    build_sdist,  # noqa: F401
+    build_wheel,  # noqa: F401
+    get_requires_for_build_wheel,  # noqa: F401
+    prepare_metadata_for_build_wheel,  # noqa: F401
 )
 
 
@@ -20,7 +20,7 @@ with _suppress(
 ):  # Only succeeds w/ setuptools implementing PEP 660
     # Re-exporting PEP 660 hooks
     from ._backend import (  # type: ignore[assignment]
-        build_editable,
-        get_requires_for_build_editable,
-        prepare_metadata_for_build_editable,
+        build_editable,  # noqa: F401
+        get_requires_for_build_editable,  # noqa: F401
+        prepare_metadata_for_build_editable,  # noqa: F401
     )
